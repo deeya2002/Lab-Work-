3dc67bcbfa608b6201f7afaeb3825b9bca07eb6f
deeya2002 <diyadangol2002@gmail.com>
2021-12-06 10:29:15 +0545

    initial commit

--- Day3/lab9.py
+++ Day3/lab9.py
@@ -1,3 +1,16 @@
 # Take username and password from user and check it again for the three times whether the user has entered the right
 # username and password. If yes then print a message "Logged in Successfully", if not then print invalid credentials.
-user
+username = input("Enter your username:")
+password = input("Enter your password:")
+for i in range(0,3):
+    print("enter your credintials?")
+    username1 = input("Enter your username:")
+    password1 = input("Enter your password:")
+    if username==username1 and password==password1:
+        print("You are sucessefully logged.")
+        break
+    else:
+        if(username!=username1 and password!=password1):
+            print("Invalid Credintials")
+else:
+    print("3 attempt finished")
